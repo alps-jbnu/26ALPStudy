87c524da8f703aa031695e78ecbbe7f2e965f10a
jjert9070 <202446129@jbnu.ac.kr>
2026-03-26 23:48:09 +0900

    Update3

---
+++ 2026-1/Basic/jjert9070/9498.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int score;
+    cin >> score;
+    
+    if(score > 90)
+        cout << 'A' << '\n';
+    else if
+}
