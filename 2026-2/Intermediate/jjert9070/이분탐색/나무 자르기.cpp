eec6a62198924929da76c2d90cbe71245dd5f169
jjert9070 <202446129@jbnu.ac.kr>
2026-09-18 10:54:05 +0900

    update

--- 2026-2/Intermediate/jjert9070/이분탐색/나무 자르기.cpp
+++ 2026-2/Intermediate/jjert9070/이분탐색/나무 자르기.cpp
@@ -2,70 +2,73 @@
 #include <vector>
 #include <algorithm>
 
-int getMaxHeight(std::vector<int>& arr, int m)
+
+long long sumFunction(std::vector<long long>& arr, long long select)
 {
-    int sum = 0;
-    int diff;
-    int selectMin = *(std::min_element(arr.begin(), arr.end()));
-    int selectMax = *(std::max_element(arr.begin(), arr.end()));
-
-    int now = selectMin;
-
-    while(true)
+    long long sum = 0;
+    for(size_t i = 0; i < arr.size(); i++)
     {
-        for(size_t i = 0; i < arr.size(); i++)
+        if(arr[i] >= select)
         {
-            if(arr[i] >= now)
-            {
-                sum = sum + (arr[i] - now);
-            }
+            sum = sum + (arr[i] - select);
         }
-
-        if(sum == 0)
-        {
-            return -1;
-        }
-
-
-        if(sum == m)
-        {
-            return now;
-        }
-        else if(sum < m)
-        {
-            sum = 0;
-            now = now + 1;
-        }
-
     }
 
-
-
+    return sum;
 }
+
+
+long long getLowerBoundHeight(std::vector<long long>& arr, long long target)
+{
+    long long result;
+    long long ans;
+    long long start = 0;
+    long long end = arr[arr.size() - 1];
+    long long mid;
+
+    while(start <= end)
+    {
+        mid = (start + end) / 2;
+        result = sumFunction(arr, mid);
+        if(result >= target)
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else if(result < target)
+        {
+            end = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
 
 
 
 int main()
 {
-    int n;
-    int m;
-    int result;
-    std::vector<int> arr;
+    std::ios::sync_with_stdio(NULL);
+    std::cin.tie(0);
+    long long n;
+    long long m;
+    long long res;
+    std::vector<long long> arr;
     std::cin>>n>>m;
 
     arr.resize(n);
-
-
 
     for(size_t i = 0; i < n; i++)
     {
         std::cin>>arr[i];
     }
 
+    std::sort(arr.begin(), arr.end());
 
-    result = getMaxHeight(arr, m);
+    res = getLowerBoundHeight(arr, m);
 
-    std::cout<<result<<'\n';
+    std::cout<<res<<'\n';
     
 
     return 0;
